e92ccc864d5249d24c7496fd88a6f2b4b455fe51
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-08 01:16:24 +0530

    ADDED: program to perform prime number and palindrome

--- src/com/bridgelabz/primenumber/PrimeNumber.java
+++ src/com/bridgelabz/primenumber/PrimeNumber.java
@@ -5,31 +5,45 @@ import java.util.Scanner;
 public class PrimeNumber {
 	public static void main (String[] args)
 	   {		
-	      Scanner scanner = new Scanner(System.in);
-	      int i =0;
-	      int num =0;
-	      //Empty String
-	      String  primeNumbers = "";
-	      System.out.println("Enter the value of n:");
-	      int n = scanner.nextInt();
-	      scanner.close();
-	      for (i = 1; i <= n; i++)  	   
-	      { 		 		  
-	         int counter=0; 		  
-	         for(num =i; num>=1; num--)
-	         {
-		    if(i%num==0)
-		    {
-			counter = counter + 1;
-		    }
-		 }
-		 if (counter ==2)
-		 {
-		    //Appended the Prime number to the String
-		    primeNumbers = primeNumbers + i + " ";
-		 }	
-	      }	
-	      System.out.println("Prime numbers from 1 to n are :");
-	      System.out.println(primeNumbers);
-	   }
-}
+		System.out.println("\nPalindrome Numbers 1 to 1000 >>");  
+		  for (int i = 1; i <= 1000; i++) {  
+		   if (Palindrome(i) == true) {  
+		    System.out.print(i + " ");  
+		   }  
+		  }  
+		  System.out.println("\nPrime Numbers 1 to 1000 >>");  
+		  for (int i = 1; i <= 1000; i++) {  
+		   if (Prime(i) == true) {  
+		    System.out.print(i + " ");  
+		   }  
+		  }  
+		 }   
+	 public static boolean Palindrome(int num) {  
+		  int num1 = num;  
+		  int rem;  
+		  int result = 0;  
+		  while (num > 0) {  
+		   rem = num % 10;  
+		   num = num / 10;  
+		   result = (result + rem) * 10;  
+		  }  
+		  result /= 10;  
+		  if (result == num1) {  
+		   return true;  
+		  } else {  
+		   return false;  
+		  }  
+		 }  
+		 public static boolean Prime(int num) {  
+		  if (num < 2) {  
+		   return false;  
+		  }  
+		  int div = num / 2;  
+		  for (int i = 2; i <= div; i++) {  
+		   if (num % i == 0) {  
+		    return false;  
+		   }  
+		  }  
+		  return true;  
+		 }  
+		} 
