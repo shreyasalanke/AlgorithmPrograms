1b9e055f08605ac5690b233f22e062a3dbe966b2
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-07 13:08:21 +0530

    ADDED: program to perfrom insertion sort of string

---
+++ src/com/bridgelabz/insertionsort/InsertionSort.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.insertionsort;
+import java.util.*;
+
+public class InsertionSort {
+	static void sort(String []s, int n)
+	{
+	    for (int i=1 ;i<n; i++)
+	    {
+	        String temp = s[i];
+	 
+	        // Insert s[j] at its correct position
+	        int j = i - 1;
+	        while (j >= 0 && temp.length() < s[j].length())
+	        {
+	            s[j+1] = s[j];
+	            j--;
+	        }
+	        s[j+1] = temp;
+	    }
+	}
+	 
+	// Function to print the sorted array of string
+	static void printArraystring(String str[], int n)
+	{
+	    for (int i=0; i<n; i++)
+	        System.out.print(str[i]+" ");
+	}
+	 
+	// Driver function
+	public static void main(String args[])
+	{
+	    String []arr = {"Shreya", "My", "Name", "is"};
+	    int n = arr.length;
+	     
+	    // Function to perform sorting
+	    sort(arr,n);
+	    // Calling the function to print result
+	    printArraystring(arr, n);
+	     
+	}
+}
